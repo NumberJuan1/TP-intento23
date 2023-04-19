36ee4ae1c13d5c468718cde8cfb27e425a3d9198
NumberJuan1 <juanbruno18jb@gmail.com>
2023-04-18 22:24:20 -0300

    actu final

--- app.js
+++ app.js
@@ -1,36 +1,71 @@
-const header = document.querySelector("#header");
-const contenedor = document.querySelector("#contenedor");
-const body = document.querySelector("body");
-window.addEventListener("scroll", function(){
-    if(contenedor.getBoundingClientRect().top<10){
-        header.classList.add("scroll")
-    }
-    else{
-        header.classList.remove("scroll")
-    }
-})
 
-function mostrar(){
-var formulario = document.getElementById('formu');
-formulario.style.display = block;
-}
-
- 
 function comprobantepago() {
-            // Obtener los valores del formulario
             var nombre = document.getElementById('nombre').value;
             var apellido = document.getElementById('apellido').value;
             var email = document.getElementById('email').value;
+            var deb = document.getElementById('tarj').value;
 
-            // Crear un nuevo documento PDF
             var doc = new jsPDF();
 
-            // Agregar contenido al PDF
-            doc.text(20, 20, 'Formulario de ejemplo');
+            doc.text(20, 20, 'Muchas gracias por tu compra. ',);
             doc.text(20, 30, 'Nombre: ' + nombre);
             doc.text(20, 40, 'Apellido: ' + apellido);
             doc.text(20, 50, 'Email: ' + email);
+            doc.text(20, 60, 'Numero tarjeta: ' + deb);
 
-            // Guardar el PDF como un archivo
-            doc.save('formulario.pdf');
+            doc.save('Recibo_Pago.pdf');
         }
+        
+        
+function mostrar(){
+    document.getElementById('formu').style.display = 'block';
+}
+
+
+
+ $('#mejora').validate({
+        rules: {
+            nombre: 'required',
+            email: {
+                required: true,
+                email: true
+            },
+            mensaje: 'required'
+        },
+        messages: {
+            nombre: 'Por favor ingrese su nombre',
+            email: {
+                required: 'Por favor ingrese su dirección de correo electrónico',
+                email: 'Por favor ingrese una dirección de correo electrónico válida'
+            },
+            mensaje: 'Por favor ingrese un mensaje'
+        },
+        submitHandler: function(form) {
+            var nombre = $('#nombre').val();
+            var email = $('#email').val();
+            var mensaje = $('#mensaje').val();
+
+            $.ajax({
+                url: 'https://reqres.in/api/users?page=2', 
+                method: 'POST', 
+                data: {
+                    nombre: nombre,
+                    email: email,
+                    mensaje: mensaje
+                },
+                success: function(response) {
+                    console.log('Éxito:', response);
+                    alert('¡Mensaje enviado con éxito!');
+                },
+                error: function(xhr, status, error) {
+                    console.error('Error:', error);
+                    alert('Error al enviar el mensaje. Por favor inténtelo nuevamente.');
+                }
+            });
+        }
+    });
+
+
+ 
+
+
